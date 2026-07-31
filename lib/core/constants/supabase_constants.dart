d1bb129fbb13b84813ab6f542e240aa08e997c57
Damedjeu Zidane <68597731+damedjeu@users.noreply.github.com>
2026-07-31 10:51:01 +0100

    Create supabase_constants.dart

---
+++ lib/core/constants/supabase_constants.dart
@@ -0,0 +1,7 @@
+class SupabaseConstants {
+  static const String supabaseUrl =
+      'https://wgctflgkhcctjxobbspn.supabase.co';
+
+  static const String supabaseAnonKey =
+      'sb_publishable_AJ65LwL9Y_7qonnt6d_5vQ_lChSZr-a';
+}
