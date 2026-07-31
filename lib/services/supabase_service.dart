a9e47094f2321b1c5d69de03e44812ef58cbb763
Damedjeu Zidane <68597731+damedjeu@users.noreply.github.com>
2026-07-31 15:49:13 +0100

    Update supabase_service.dart

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -1,1 +1,38 @@
 
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseService {
+  static final SupabaseClient client = Supabase.instance.client;
+
+  /// Connexion
+  static Future<AuthResponse> signIn({
+    required String email,
+    required String password,
+  }) async {
+    return await client.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  /// Inscription
+  static Future<AuthResponse> signUp({
+    required String email,
+    required String password,
+  }) async {
+    return await client.auth.signUp(
+      email: email,
+      password: password,
+    );
+  }
+
+  /// Déconnexion
+  static Future<void> signOut() async {
+    await client.auth.signOut();
+  }
+
+  /// Utilisateur connecté
+  static User? get currentUser {
+    return client.auth.currentUser;
+  }
+}
