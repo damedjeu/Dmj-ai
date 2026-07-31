fb37b4de09d1468aa9677726c53eeec164c8dfd4
Damedjeu Zidane <68597731+damedjeu@users.noreply.github.com>
2026-07-31 06:49:01 +0100

    Create app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Couleur principale DMJ AI
+  static const Color primary = Color(0xFF0A84FF);
+
+  // Couleur secondaire
+  static const Color secondary = Color(0xFF1E1E1E);
+
+  // Arrière-plan
+  static const Color background = Color(0xFF050505);
+
+  // Texte
+  static const Color white = Colors.white;
+  static const Color grey = Colors.grey;
+
+  // États
+  static const Color success = Color(0xFF34C759);
+  static const Color warning = Color(0xFFFF9500);
+  static const Color error = Color(0xFFFF3B30);
+}
